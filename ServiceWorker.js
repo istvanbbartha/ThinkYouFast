00fceb9ca528bb0467caaa57f558c0f787d1b527
Ivan Bartha <ivanbbartha@gmail.com>
2025-06-26 09:15:22 -0700

    icons

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "OCMotoAcademy-ThinkYouFast-0.1";
+const contentToCache = [
+    "Build/ThinkYouFast.loader.js",
+    "Build/ThinkYouFast.framework.js",
+    "Build/ThinkYouFast.data",
+    "Build/ThinkYouFast.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
